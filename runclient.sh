0e1ef8bd7361390535cf8511040a496ece701004
Daniel Estévez G <daniel@daniel.waripolo>
2010-10-03 23:10:03 -0400

    Se agregan librerias y se modifican scripts para que apunten a estas

--- runclient.sh
+++ runclient.sh
@@ -6,7 +6,7 @@
 #or without aspectj aspects woven in
 
 #version with aj aspects woven in -- be sure this is the correct path to aspectjrt.jar!
-cpath=bin:/home/daniel/DEV/workspace/org.eclipse.swt/swt.jar:/home/daniel/Programas/eclipse/plugins/org.aspectj.runtime_1.6.9.20100629172100/aspectjrt.jar
+cpath=bin:lib/swt.jar:lib/aspectjrt.jar
 
 #version without aj aspects woven in
 #cpath=bin
